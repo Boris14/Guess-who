214acc5a52ffa58872a16fb1cc8f648b86e4fb7e
Boris14 <boris1234dimitrov@gmail.com>
2020-03-28 15:26:50 +0200

    Add files via upload

--- Server/main.lua
+++ Server/main.lua
@@ -1,22 +1,33 @@
-local socket = require('socket')
-udp = socket.udp()
-udp:setsockname('*', 12345)
-udp:settimeout(0)
+local enet = require "enet"
 
-local new_image = ""
+local host = enet.host_create"localhost:5678"
+
+pixels = ""
+while true do
+	local event = host:service(1)
+	if event then 
+		if event.type == "receive" then
+			--print("Got message: ",  event.data, event.peer)
+			--event.peer:send("howdy back at ya")
+			pixels = pixels .. event.data
+		elseif event.type == "connect" then
+			print("Connect:", event.peer)
+			host:broadcast("new client connected")
+		else
+			print("Got event", event.type, event.peer)
+			if (event.type == "disconnect") then
+				print(string.len(pixels))
+				newImageData = love.image.newImageData(300, 300, "rgba8", pixels)
+				image = love.graphics.newImage( newImageData )
+				break
+			end
+		end
+
+	end
+end
 
 function love.draw()
-	if(new_image ~= "") then
-		love.graphics.draw(new_image)
+	if(image) then
+		love.graphics.draw(image)
 	end
 end
-
-function love.update()
-	data, msg_or_ip, port_or_nil = udp:receivefrom()
-	if data then
-		local image = data
-		new_image = love.graphics.newImage(data)
-	end
-end
-
-
