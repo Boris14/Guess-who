c7a588ff517f1a785373c58036afe4c45c22fd6f
Boris14 <boris1234dimitrov@gmail.com>
2020-04-04 14:41:33 +0300

    Add files via upload
    made functions for sending and receiving images and for showing all .png files in a directory

--- Client/main.lua
+++ Client/main.lua
@@ -1,24 +1,72 @@
 local enet = require "enet"
+require "torch"
 
 local host = enet.host_create()
 local server = host:connect("localhost:5678")
 
-imageData = love.image.newImageData("fff.png")
-pixels = imageData:getString()
+--		funtion to send an image by its name by
+--		breaking it down to pixels and sending
+--		them as a string 
+function sendImage(imageName)
 
-print(string.len(pixels))
-local buff = ""
-for i = 1, #pixels do
-    local c = pixels:sub(i,i)
-	buff = buff .. c
-	if(string.len(buff) > 9000 or i == #pixels) then
-		local event = host:service(1)		
-		server:send(buff)
-		buff = ""
+	local imageData = love.image.newImageData(imageName)
+	local pixels = imageData:getString()
+	local imageWidth = imageData:getWidth()
+	local imageHeight = imageData:getHeight()
+
+	local event = host:service(1)
+	server:send(tostring(imageWidth))
+	local event = host:service(1)
+	server:send(tostring(imageHeight)) 
+
+	local buff = ""
+	for i = 1, #pixels do
+		local c = pixels:sub(i,i)
+		buff = buff .. c
+		if(string.len(buff) > 9000 or i == #pixels) then
+			local event = host:service(1)		
+			server:send(buff)
+			buff = ""
+		end
 	end
+
+	local event = host:service(1)	
+	server:send("end")
+
 end
+--		end of sendImage(imageName)
+
+
+--		function to send all the images from the Client/
+--		directory using sendImage(imageName)
+function sendAllImages()
+	for file in paths.files("Client/") do
+		is_image = false
+		for i = #file,1,-1 do
+			local a = file:sub(i,i)
+			local b = file:sub(i-1,i-1)
+			local c = file:sub(i-2,i-2)
+			local d = file:sub(i-3,i-3)
+			if(a == 'g' and b == 'n' and c == 'p' and d == '.') then
+				is_image = true
+				break
+			else
+				break
+			end
+		end
+
+		if(is_image) then
+			sendImage(file)
+		end
+	end
+
+end
+--		end of sendAllImages()
+
+sendAllImages()
+
 local event = host:service(1)	
-server:send("test")
+server:send("it doesn't work without this send")
 
 server:disconnect()
 host:flush()
--- Server/main.lua
+++ Server/main.lua
@@ -2,32 +2,65 @@ local enet = require "enet"
 
 local host = enet.host_create"localhost:5678"
 
-pixels = ""
-while true do
-	local event = host:service(1)
-	if event then 
-		if event.type == "receive" then
-			--print("Got message: ",  event.data, event.peer)
-			--event.peer:send("howdy back at ya")
-			pixels = pixels .. event.data
-		elseif event.type == "connect" then
-			print("Connect:", event.peer)
-			host:broadcast("new client connected")
-		else
-			print("Got event", event.type, event.peer)
-			if (event.type == "disconnect") then
-				print(string.len(pixels))
-				newImageData = love.image.newImageData(300, 300, "rgba8", pixels)
-				image = love.graphics.newImage( newImageData )
-				break
-			end
-		end
 
-	end
+newImages = {}
+
+function sleep(n)
+  os.execute("sleep " .. tonumber(n))
 end
 
 function love.draw()
-	if(image) then
-		love.graphics.draw(image)
+	for i = 1, #newImages do
+		love.graphics.draw(newImages[i])
+		--sleep(2)
 	end
 end
+
+
+--		function to recieve an image sent by sendImage(imageName)
+--		and returning it or returning nil if no image is sent 
+function recieveImage()
+	local pixels = ""
+	local imageWidth = 0
+	local imageHeight = 0
+	local image = nil
+	while true do
+		local event = host:service(1)
+		if event then
+			if event.type == "receive" then
+				if(imageWidth == 0) then
+					imageWidth = tonumber(event.data)
+				elseif(imageHeight == 0) then
+					imageHeight = tonumber(event.data)
+				elseif(event.data ~= "end") then
+					pixels = pixels .. event.data
+				else
+					local newImageData = love.image.newImageData(imageWidth, imageHeight, "rgba8", pixels)
+					image = love.graphics.newImage( newImageData )					
+					break
+				end
+			elseif event.type == "connect" then
+				print("Connect:", event.peer)
+				host:broadcast("new client connected")
+			else
+				print("Got event", event.type, event.peer)
+				if (event.type == "disconnect") then
+					break
+				end
+			end
+		end
+	end
+	return image 
+end
+--		end of recieveImage()
+
+while true do
+	newData = recieveImage()
+	if(newData) then
+		table.insert(newImages, newData)
+	else
+		break
+	end
+end
+
+
