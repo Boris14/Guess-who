2f1d2f1b176db5fa0eea29a514fbdd4fd8776eae
Boris14 <boris1234dimitrov@gmail.com>
2020-04-17 10:52:44 +0300

    showing the position of the faces, when the total number is from 1 to 25

--- test/main.lua
+++ test/main.lua
@@ -1,25 +1,43 @@
 image = love.graphics.newImage("cat.png")
-faces = 20
+faces = 1
+rowsDistance = faces / 5
 
+function love.load()
+	timer = 0
+	timerLimit = 1
+end
+
+function love.update(dt)
+	timer = timer + dt
+	if(faces < 25) then
+		if(timer >= timerLimit) then
+			timer = timer - timerLimit
+			faces = faces + 1
+			if(faces % 5 == 1) then
+				rowsDistance = (faces + 4) / 5
+			end
+		end	
+	end
+
+end
 
 function love.draw()
 	love.graphics.setBackgroundColor(1, 1, 1)
 	love.graphics.push()
 	for i = 1, faces do
-		love.graphics.draw(image, 40, 10, 0, 0.5)
+		love.graphics.draw(image, 40, 0, 0, 0.5)
 		if(i % 5 == 0) then
 			love.graphics.pop()
 			if(i < faces) then
-				love.graphics.translate(0, 100)
-			else
-				love.graphics.translate(0, 75)
+				love.graphics.translate(0, 400/rowsDistance)
 			end
 			love.graphics.push()
 		else
 			love.graphics.translate(150, 0)		
 		end
 	end
-	love.graphics.translate(235, 0)
-	love.graphics.draw(image, 40, 10)
+	love.graphics.origin()
+	love.graphics.translate(280, 380)
+	love.graphics.draw(image)
 	love.graphics.pop()
 end 
