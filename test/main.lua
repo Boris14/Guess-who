b517a191329edc5c54b299d5ab2a6d79e8d483a1
Boris14 <boris1234dimitrov@gmail.com>
2020-04-17 09:54:17 +0300

    adding up to 20 faces + your face

--- test/main.lua
+++ test/main.lua
@@ -1,5 +1,5 @@
 image = love.graphics.newImage("cat.png")
-faces = 15
+faces = 20
 
 
 function love.draw()
@@ -9,13 +9,17 @@ function love.draw()
 		love.graphics.draw(image, 40, 10, 0, 0.5)
 		if(i % 5 == 0) then
 			love.graphics.pop()
-			love.graphics.translate(0, 150)
-			if(i < 15) then
-				love.graphics.push()
+			if(i < faces) then
+				love.graphics.translate(0, 100)
+			else
+				love.graphics.translate(0, 75)
 			end
+			love.graphics.push()
 		else
 			love.graphics.translate(150, 0)		
 		end
 	end
-
+	love.graphics.translate(235, 0)
+	love.graphics.draw(image, 40, 10)
+	love.graphics.pop()
 end 
